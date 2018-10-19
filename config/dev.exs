6ec73a3819ace6f642e3a3ad28d0347b629d51e5
zachdaniel <zachary.s.daniel@gmail.com>
2018-10-19 16:57:33 -0400

    chore: only dev needs git_ops configuration

---
+++ config/dev.exs
@@ -0,0 +1,13 @@
+use Mix.Config
+
+config :git_ops,
+  mix_project: SpandexDatadog.MixProject,
+  changelog_file: "CHANGELOG.md",
+  repository_url: "https://github.com/spandex-project/spandex_datadog",
+  types: [],
+  # Instructs the tool to manage your mix version in your `mix.exs` file
+  # See below for more information
+  manage_mix_version?: true,
+  # Instructs the tool to manage the version in your README.md
+  # Pass in `true` to use `"README.md"` or a string to customize
+  manage_readme_version: "README.md"
