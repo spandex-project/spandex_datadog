b80a876b63d0a3557e5fbfc8f4219de5e6504888
Dallin Crane <10406163+dallincrane@users.noreply.github.com>
2021-05-04 10:45:55 -0600

    update regex

--- lib/spandex_datadog/api_server.ex
+++ lib/spandex_datadog/api_server.ex
@@ -106,7 +106,7 @@ defmodule SpandexDatadog.ApiServer do
   @cgroup_uuid "[0-9a-f]{8}[-_][0-9a-f]{4}[-_][0-9a-f]{4}[-_][0-9a-f]{4}[-_][0-9a-f]{12}"
   @cgroup_ctnr "[0-9a-f]{64}"
   @cgroup_task "[0-9a-f]{32}-\\d+"
-  @cgroup_regex Regex.compile!("/(#{@cgroup_uuid}|#{@cgroup_ctnr}|#{@cgroup_task})(?:.scope)?$", "m")
+  @cgroup_regex Regex.compile!(".*(#{@cgroup_uuid}|#{@cgroup_ctnr}|#{@cgroup_task})(?:\\.scope)?$", "m")
 
   defp get_container_id() do
     with {:ok, file_binary} <- File.read("/proc/self/cgroup"),
